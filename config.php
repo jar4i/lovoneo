73f806ab524509c32096afd23d0317171834bcf9
root <root@lovoneo.com>
2018-07-17 08:36:42 +0000

    search_form changed

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 define( "DB_DSN", "mysql:host=localhost;dbname=projekt" );
-define( "DB_USERNAME", "root" );
-define( "DB_PASSWORD", "Asal4Asal41" );
+define( "DB_USERNAME", "lovoneo" );
+define( "DB_PASSWORD", "ZMaLPF2-unV-ch" );
 ?>
