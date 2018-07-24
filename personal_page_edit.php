60f05ed035300fc2e7fae25afa7fa59acefc151b
KseniyaOsadcha <theksenic@gmail.com>
2018-07-24 12:06:30 +0300

    Edit style for like

--- personal_page_edit.php
+++ personal_page_edit.php
@@ -7,7 +7,7 @@
     <div class="wrap">
         <nav class="pull_left">
             <ul class="list-unstyled ">
-            <a class="active" href="index.php"><li class="li-item inline-block">Home</li></a>
+              <a class="active" href="index.php"><li class="li-item inline-block">Home</li></a>
               <a href="#news"><li class="li-item inline-block">News</li></a>
               <a href="#contact"><li class="li-item inline-block">Contact</li></a>
               <a href="#about"><li class="li-item inline-block">About</li></a>
@@ -16,10 +16,9 @@
         <div class="pull-right rel">
             <a class="welcom" href="view_profile.php"> <?php 
                 if (isset($_SESSION['user_name'])) {
-                    echo "<div class='profile_photo_menu_box inline-block'><img class='profile_photo_menu' src='".$_SESSION['profile_foto']."'> </div>";
+                   echo "<div class='profile_photo_menu_box inline-block'><img class='profile_photo_menu' src='".$_SESSION['profile_foto']."'> </div>";
                    echo ''.$_SESSION['first_name'];
                    echo '  |';
-                
                 }
                 ?>
             </a>
@@ -27,7 +26,6 @@
                 if (isset($_SESSION['user_name'])) {
                     echo'<a href="logout.php">Log out</a>';
                 }
-                else echo '<a href="login.php">Log in</a>';
                 ?>
         </div>
     </div>
@@ -35,7 +33,6 @@
 
 
 <?php
-
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 'On');
 if(empty($_GET["user_activation_code"])){
