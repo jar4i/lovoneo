6e48c600c447c25ad347b57eec3a032ff6eb6517
KseniyaOsadcha <theksenic@gmail.com>
2018-07-30 17:57:46 +0300

    Change colors in User On page

--- user_on.php
+++ user_on.php
@@ -1,36 +1,9 @@
 <?php
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 'On');
-//if(isset($_SESSION['user_name']))
-//{
-//echo 'Welcome, '.$_SESSION['user_name'];
-//echo '<br>';
-//echo '<a href="logout.php">Logout</a>';
-//}
-?>
-<!--
-<div class="user_on">
-<?php
-error_reporting(E_ALL | E_STRICT);
-ini_set('display_errors', 'On');
-include("connection.php");
 
-$user_name = $_SESSION['user_name'];
-$query = "SELECT * FROM register_user WHERE user_name = '$user_name'";
-$mysql_result = mysqli_query($conn, $query);
 ?>
 
-<?php
-while ($row = mysqli_fetch_assoc($mysql_result)) :
-?>
-
-<a>Name:	</a><?php echo $row['first_name']; ?><br>
-<a>City:	</a><?php echo $row['city'];?><br>
-<a>Country:	</a><?php echo $row['country'];?><br>
-<a>Profile foto	</a><img src = "<?php echo $row['profile_foto'];?>"><br>
-<a href="personal_page_edit.php?user_activation_code=<?php echo $row['user_activation_code'];?>&&user_id=<?php echo $row['user_id'];?>">Go to personal page</a>
-<?php endwhile; ?>
-</div>-->
 
 <!DOCTYPE html>
 <html>
@@ -65,17 +38,17 @@ while ($row = mysqli_fetch_assoc($mysql_result)) :
                     <div class="buttons inline-bock">
                         <ul class="accordion-menu">
                           <li>
-                            <div class="dropdownlink dropdownlink_1">
+                            <div class="dropdownlink dropdownlink_4">
                             <a href="view_profile.php">Profile</a>
                             </div>
                           </li>
                           <li>
-                            <div class="dropdownlink dropdownlink_2">
+                            <div class="dropdownlink dropdownlink_4">
                             <a href="message1/message.php">Mеssages</a>
                             </div>
                           </li>
                           <li>
-                            <div class="dropdownlink dropdownlink_3">
+                            <div class="dropdownlink dropdownlink_4">
                             <a href="personal_page_edit.php?user_activation_code=<?php echo $row['user_activation_code'];?>&&user_id=<?php echo $row['user_id'];?>">Edit profile</a>
                             </div>
                           </li>
