3d597a5f31f491c50480c6828ebc9d52e0ed60b4
KseniyaOsadcha <theksenic@gmail.com>
2018-07-31 16:54:32 +0300

    Edit Log in style

--- login.php
+++ login.php
@@ -77,7 +77,7 @@ if(isset($_POST["login"]))
 						</div>
 
 					</form>
-					<div class="register-link"><a href="register.php" > Register</a></div><br>
+					<div class="btn btn-danger register-link"><a href="register.php" > Register</a></div><br>
 					<div class="fg-pass"><a href="password_restore.php" >Forgot your password?</a></div><br>
 					
 				</div>
