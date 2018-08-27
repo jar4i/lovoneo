0b90c4531cc4552989924f2f73004c3b713df56c
KseniyaOsadcha <theksenic@gmail.com>
2018-08-27 17:36:50 +0300

    change cropper

--- personal_page_edit.php
+++ personal_page_edit.php
@@ -5,7 +5,6 @@
     <link rel="stylesheet" href="personal_page_edit.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css">
 		<link rel="stylesheet" href="menu.css">
-    <link rel="stylesheet" href="cropper.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
 </head>
 <?php
@@ -59,6 +58,7 @@ echo $update_img;
 ?>
 <?php
 $user_activation_code= $_GET["user_activation_code"];
+$u_id = $_SESSION['us_id'];
 if(isset($_POST["save"])) {
 $birth_date = $_POST['birth_date'];
 $weight = $_POST['weight'];
@@ -150,74 +150,17 @@ $sthandler->execute();
         <div class="wrap">
     <?php while($row = $sthandler->fetch(PDO::FETCH_ASSOC)) : ?>
         <div class="container">
-            <form class ="button_foto" action="" method="post" enctype="multipart/form-data">
-                <div class="clearfix_card rel">
-                    <div class="prof_photo_box">
-                        <img class="rounded prof_photo" id="avatar" src = "<?php echo $row ['profile_foto']?>" alt="avatar">
-                    </div>
-                        <input type="file"  id="input" name="fileToUpload" >
-                        <label for="input"  class="text_edit_box"> 
+        <form class ="button_foto" action="" method="post" enctype="multipart/form-data">
+                    <div class="clearfix_card rel">
+                        <div class="prof_photo_box">
+                            <img class="rounded prof_photo" id="avatar" src = "<?php echo $row ['profile_foto']?>" alt="avatar">
+                        </div>
+                        <a href="fucking_shit.php?user_activation_code=<?php echo $user_activation_code; ?>&&user_id=<?php echo $u_id;?>"  class="text_edit_box"> 
                             <div class="btn-add-photo"><i class="fas fa-camera"></i></div>
                             <div class="text_edit"> <?php echo $array[22];?></div>
-                        </label>
-                </div>
-                <div class="alert" role="alert"></div>
-                <div class="modal" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="modalLabel"><?php echo $array[23];?></h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <div class="img-container">
-                            <img id="image" src="image_crop/default.jpg">
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-primary" id="zoom-in" title="Zoom In">
-                                    <span class="docs-tooltip" data-toggle="tooltip" >
-                                    <span class="fa fa-search-plus"></span>
-                                    </span>
-                                </button>
-                                <button type="button" class="btn btn-primary" id="zoom-out" title="Zoom Out">
-                                    <span class="docs-tooltip" data-toggle="tooltip" >
-                                    <span class="fa fa-search-minus"></span>
-                                    </span>
-                                </button>
-                            </div>
-                            <div class="btn-group btn-group-footer">
-                                <button type="button" class="btn btn-primary" id="move-left" title="Move Left">
-                                    <span class="docs-tooltip" data-toggle="tooltip" >
-                                    <span class="fa fa-arrow-left"></span>
-                                    </span>
-                                </button>
-                                <button type="button" class="btn btn-primary" id="move-right" title="Move Right">
-                                    <span class="docs-tooltip" data-toggle="tooltip" >
-                                    <span class="fa fa-arrow-right"></span>
-                                    </span>
-                                </button>
-                                <button type="button" class="btn btn-primary" id="move-up" title="Move Up">
-                                    <span class="docs-tooltip" data-toggle="tooltip">
-                                    <span class="fa fa-arrow-up"></span>
-                                    </span>
-                                </button>
-                                <button type="button" class="btn btn-primary" id="move-down" title="Move Down">
-                                    <span class="docs-tooltip" data-toggle="tooltip" s>
-                                    <span class="fa fa-arrow-down"></span>
-                                    </span>
-                                </button>
-                            </div>
-                            <button type="button" class="btn btn-default " data-dismiss="modal">Cancel</button>
-                            <button type="button" class="notablock" ><?php echo $array[43];?></button>
-                            <input type="submit" value="<?php echo $array[44];?>" id="crop" class="btn btn-primary" name="submit">
-                        </div>
+                        </a>
                     </div>
-                </div>
-            </form>
+                </form>
         </div>
         </div>
         </div>
@@ -330,122 +273,7 @@ $sthandler->execute();
     </section>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.bundle.min.js"></script>
-  <script src="cropper.js"></script>
   
-  <script>
-      
-    window.addEventListener('DOMContentLoaded', function () {
-      var avatar = document.getElementById('avatar');
-      var image = document.getElementById('image');
-      var input = document.getElementById('input');
-      var moveLeft = document.getElementById('move-left');
-      var moveRight = document.getElementById('move-right');
-      var moveUp = document.getElementById('move-up');
-      var moveDown = document.getElementById('move-down');
-      var zoomIn = document.getElementById('zoom-in');
-      var zoomOut = document.getElementById('zoom-out');
-      var $alert = $('.alert');
-      var $modal = $('#modal');
-      var cropper;
-      $('[data-toggle="tooltip"]').tooltip();
-      
-      moveLeft.addEventListener('click', function(){
-        cropper.move(4, 0);
-      })
-      moveRight.addEventListener('click', function(){
-        cropper.move(-4, 0);
-      })
-      moveUp.addEventListener('click', function(){
-        cropper.move(0, 4);
-      })
-      moveDown.addEventListener('click', function(){
-        cropper.move(0, -4);
-      })
-      zoomIn.addEventListener('click', function(){
-        cropper.zoom(0.1);
-      })
-      zoomOut.addEventListener('click', function(){
-        cropper.zoom(-0.1);
-      })
-      input.addEventListener('change', function (e) {
-        var files = e.target.files;
-        var done = function (url) {
-          
-          image.src = url;
-          $alert.hide();
-          $modal.modal('show');
-        };
-        var reader;
-        var file;
-        var url;
-
-        if (files && files.length > 0) {
-          file = files[0];
-
-          if (URL) {
-            done(URL.createObjectURL(file));
-          } else if (FileReader) {
-            reader = new FileReader();
-            reader.onload = function (e) {
-              done(reader.result);
-            };
-            reader.readAsDataURL(file);
-          }
-        }
-      }); 
-     
-        var cook = function createCookie(x, y, w, h) {
-            document.cookie = "y=" + escape(y) 
-              + "; path=/";
-             document.cookie =  "width=" + escape(w) 
-             +  "; path=/";
-             document.cookie = 
-             "height=" + escape(h)  + "; path=/";
-             document.cookie = "x=" + escape(x) 
-              + "; path=/";
-        }
-      var data = document.querySelector('#data');
-      $modal.on('shown.bs.modal', function () {
-        cropper = new Cropper(image, {
-            
-        ready: function (event) {
-          // Zoom the image to its natural size
-          cropper.zoomTo(1);
-        },
-
-        crop: function (event) {
-            cook(cropper.getData().x, cropper.getData().y, cropper.getData().width, cropper.getData().height);
-        },
-
-          dragMode: 'move',
-        aspectRatio: 200 / 250,
-        autoCropArea: 0.65,
-        restore: false,
-        });
-        
-      }).on('hidden.bs.modal', function () {
-        cropper.destroy();
-        cropper = null;
-      });
-
-      document.getElementById('crop').addEventListener('click', function () {
-        var initialAvatarURL;
-        var canvas;
-
-        $modal.modal('hide');
-
-        if (cropper) {
-          canvas = cropper.getCroppedCanvas({
-            width: 160,
-            height: 160,
-          });
-          initialAvatarURL = avatar.src;
-          avatar.src = canvas.toDataURL();
-        }
-      });
-    });
-  </script>
-
 <script type="text/javascript">
 $(document).ready(function(){
     $('._hamburger').click(function () {
